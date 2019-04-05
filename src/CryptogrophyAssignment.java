894787bf8a18cd33de6a90396dd8e2e63c7eaef0
ahance21 <42975655+ahance21@users.noreply.github.com>
2019-04-05 11:42:18 -0500

    Add files via upload

---
+++ src/CryptogrophyAssignment.java
@@ -0,0 +1,69 @@
+/*
+ * Austin Hance
+ */
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CryptogrophyAssignment {
+	
+
+	public static void main(String[] args) throws IOException{
+	{
+		
+		
+	   Scanner scan = new Scanner(System.in);
+	   
+	   System.out.print("Please enter a message to code ");
+	   String message = scan.nextLine();
+	   
+	   char[]alpha = new char [26];
+	   for (int i=0; i <= 'z' - 'a'; i++)
+	     alpha[i] = (char) (i + 'a');
+	   System.out.print("Enter a file name (absolute name): ");
+	    String filename = scan.nextLine();
+	}
+	}
+	
+	
+	
+	
+	    
+	public static boolean isLower(char c)
+	{
+		return c <= 'a' && c <= 'z';
+	
+	    
+	}
+	public static boolean isLower1(char c)
+	{
+		return c >= 'A' && c <= 'Z';
+	}
+	
+
+	  
+		  
+	
+		  
+	   
+	   
+	   {
+		   
+	   }
+		{
+	  
+	  {
+	   
+	  
+			 
+		     
+	   
+	  }
+	   
+	   
+	  }
+	   
+	   
+	   
+	}
+
+
